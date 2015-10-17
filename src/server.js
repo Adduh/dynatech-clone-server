ad7c6b0fcf56c71dcbea608f496c32df9462224c
Artur Taube <adduh@web.de>
2015-10-17 20:28:10 +0200

    Apply new codestyle and rename tests.

--- src/server.js
+++ src/server.js
@@ -1,11 +1,11 @@
-"use strict";
+'use strict';
 var net = require('net');
 
 var Server = function constructor() {
   this.server = null;
 };
 
-Server.prototype.start = function (port, callback) {
+Server.prototype.start = function(port, callback) {
   if (typeof port === 'undefined') {
     port = 5000;
   }
@@ -14,17 +14,17 @@ Server.prototype.start = function (port, callback) {
   }
   var server = net.createServer(function(c) {
     console.log('new client connected!');
-    c.on('end', function () {
+    c.on('end', function() {
       console.log('client disconnected!');
     });
   });
   server.listen(port, callback);
   this.server = server;
-}
+};
 
-Server.prototype.stop = function (callback) {
+Server.prototype.stop = function(callback) {
   this.server.close(callback);
   this.server = null;
-}
+};
 
 module.exports = { Server: Server };
--- test/server.js
+++ test/server.js
@@ -3,14 +3,14 @@ require('blanket');
 var assert = require('assert');
 var net = require('net');
 
-var Server = require("../src/server.js").Server;
+var Server = require('../src/server.js').Server;
 describe('Server', function() {
   var server;
   beforeEach(function() {
     server = new Server();
   });
-  describe('Start Server', function() {
-    it('should start with default port: 5000', function(done) {
+  describe('start()', function() {
+    it('starts with default port: 5000', function(done) {
       server.start();
       var client = new net.connect({port: 5000}, function() {
         done();
@@ -19,7 +19,7 @@ describe('Server', function() {
       });
     });
 
-    it('should start with specific port: 5050', function(done) {
+    it('starts with specific port: 5050', function(done) {
       server.start(5050);
       var client = new net.connect({port: 5050}, function() {
         done();
@@ -28,7 +28,7 @@ describe('Server', function() {
       });
     });
 
-    it('start should throw an error when start is called twice', function() {
+    it('throws an error when start is called twice', function() {
       server.start(5050);
       assert.throws(function() {
         server.start(5050);
@@ -38,18 +38,18 @@ describe('Server', function() {
 
   });
 
-  describe('Stop Server', function() {
+  describe('stop()', function() {
     beforeEach(function() {
       server.start(5100);
     });
 
-    it('stopping should work when no client is connected', function(done) {
+    it('works when no client is connected', function(done) {
       server.stop(done);
     });
 
-    it('reopening of server should work on the same port after stopping', function(done) {
+    it('can be started on the same port after stopping', function(done) {
       server.stop(function() {
-        server.start(5100,function() {
+        server.start(5100, function() {
           done();
           server.stop();
         });
