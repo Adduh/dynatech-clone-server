8c3bad8c8b0318266fa8b0951203ba13278dfdf0
Emanuel Taube <emanuel.taube@sophos.com>
2015-12-06 11:19:32 +0100

    Adding plain Connection class to start with

---
+++ src/connection.js
@@ -0,0 +1,12 @@
+'use strict';
+var net = require('net');
+
+var Connection = function() {
+};
+
+Connection.prototype.init = function() {
+}
+
+Connection.prototype.onRecv = function() {
+}
+module.exports = Connection;
---
+++ test/connection.js
@@ -0,0 +1,12 @@
+'use strict';
+require('blanket');
+var assert = require('assert');
+var net = require('net');
+
+var Connection = require('../src/connection.js');
+describe('Connection', function() {
+  describe('recv()', function() {
+    it('gets data!', function() {
+    });
+  });
+});
