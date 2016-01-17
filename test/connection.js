69f7e8fa6c957b01b783d8a830011da95a1b4f99
Emanuel Taube <emanuel.taube@sophos.com>
2016-01-17 14:21:15 +0100

    Added test for Connection constructor

--- test/connection.js
+++ test/connection.js
@@ -1,10 +1,21 @@
 'use strict';
 require('blanket');
+var net = require('net');
+var assert = require('assert');
 
 var Connection = require('../src/connection.js');
 describe('Connection', function() {
-  describe('recv()', function() {
-    it('gets data!', function() {
+  var connection;
+  describe('constructor', function() {
+    it('registers onRecv event', function() {
+      var socket = new net.Socket();
+      assert.equal(socket.listenerCount('data'), 0);
+      connection = new Connection(socket);
+      assert.equal(socket.listenerCount('data'), 1);
+    });
+  });
+  describe('onRecv(data)', function() {
+    it('parses data!', function() {
     });
   });
 });
