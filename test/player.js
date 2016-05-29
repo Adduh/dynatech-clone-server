cb245a46f79a6a99eaa3cf9714c2e3bfce3e04f2
Artur Taube <adduh@web.de>
2016-05-29 21:05:12 +0200

    Use sinon spies.

--- test/player.js
+++ test/player.js
@@ -1,29 +1,24 @@
 'use strict';
 
 var assert = require('assert');
+var sinon = require('sinon');
 
 var Player = require('../src/player.js');
 
 describe('Player', () => {
   var player;
   var producer;
-  class TestProducer {
-    constructor() {
-      this.ticked = false;
-    }
-    tick() {
-      this.ticked = true;
-    }
-  }
   beforeEach(() => {
     player = new Player();
-    producer = new TestProducer();
+    producer = { tick: sinon.spy() };
   });
+
   describe('constructor()', () => {
     it('it initiates producers', () => {
       assert.equal(player.producers.length, 0);
     });
   });
+
   describe('addProducer()', () => {
     it('adds a producer to the producers list', () => {
       assert.equal(player.producers.length, 0);
@@ -32,13 +27,13 @@ describe('Player', () => {
       assert.equal(player.producers[0], producer);
     });
   });
+
   describe('tick()', () => {
     it('calls tick on all producers', () => {
       assert.equal(player.producers.length, 0);
       player.addProducer(producer);
       player.tick();
-      assert.equal(producer.ticked, true);
-
+      assert.equal(producer.tick.called, true);
     });
   });
 });
