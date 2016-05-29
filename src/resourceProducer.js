46f1fc82187c30e509c98b5661fa64d4aa781769
Emanuel Taube <emanuel.taube@sophos.com>
2016-05-29 21:20:49 +0200

    adding resourceProducer class

---
+++ src/resourceProducer.js
@@ -0,0 +1,13 @@
+'use strict';
+
+class ResourceProducer {
+  constructor(input, output, location) {
+    this.input = input;
+    this.output = output;
+    this.location = location;
+  }
+  tick() {
+  }
+}
+
+module.exports = ResourceProducer;
---
+++ test/resourceProducer.js
@@ -0,0 +1,27 @@
+'use strict';
+
+var assert = require('assert');
+var sinon = require('sinon');
+
+var ResourceProducer = require('../src/resourceProducer.js');
+
+describe('ResourceProducer', () => {
+  var producer;
+  var location;
+  beforeEach(() => { 
+    producer = new ResourceProducer({}, {}, 'somewhere'); 
+    location = { has: sinon.spy(), canStore: sinon.spy() };
+
+  });
+
+  describe('contructor()', () => {
+    it('initiates input, output and location', () => {
+      assert.deepEqual(producer.input, {});
+      assert.deepEqual(producer.output, {});
+      assert.equal(producer.location, 'somewhere');
+    });
+  });
+  describe('tick()', () => {
+  });
+});
+
