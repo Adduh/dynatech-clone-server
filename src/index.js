298e6a6f57017f3d78dc52f59e58444efa56ef3a
Artur Taube <adduh@web.de>
2016-05-29 19:23:06 +0200

    Fix logging of milliseconds breaking format.

--- src/index.js
+++ src/index.js
@@ -2,5 +2,9 @@
 
 exports.require = require;
 var Server = require('./server.js');
+var Game = require('./game.js');
 var server = new Server();
 server.start();
+
+var game = new Game();
+game.start();
--- src/log.js
+++ src/log.js
@@ -4,10 +4,15 @@ function log() {
   if (process.env.NODE_ENV !== 'test') {
     var args = Array.prototype.slice.call(arguments);
     var date = new Date();
-    var dateFormat = date.getHours() + ':' +
-                     date.getMinutes() + ':' +
-                     date.getSeconds() + '.' +
-                     date.getMilliseconds();
+    var ms = date.getMilliseconds();
+    var msAsThreeDigits = (ms < 10) ? '00' + ms :
+                                (ms < 100) ? '0' + ms : ms;
+    var dateFormat = date.toLocaleTimeString('de', {
+      hour12: false,
+      hour: '2-digit',
+      minute: '2-digit',
+      second: '2-digit',
+    }) + '.' + msAsThreeDigits;
 
     args[0] = dateFormat + ' | ' + args[0];
     console.log.apply(null, args);
