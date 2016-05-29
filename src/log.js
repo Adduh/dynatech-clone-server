20d0b19bd39f5eae55f6feb1a6dd1498aebd4fdc
Artur Taube <adduh@web.de>
2016-05-29 15:17:39 +0200

    Add timestamps to logs.

--- src/log.js
+++ src/log.js
@@ -1,6 +1,14 @@
 function log() {
   if(process.env.NODE_ENV !== 'test') {
-    console.log.apply(this, arguments);
+    var args = Array.prototype.slice.call(arguments);
+    var date = new Date();
+    var dateFormat = date.getHours() + ":" +
+                     date.getMinutes() + ":" +
+                     date.getSeconds() + "." +
+                     date.getMilliseconds();
+
+    args[0] = dateFormat + " | " + args[0];
+    console.log.apply(this, args);
   }
 }
 
