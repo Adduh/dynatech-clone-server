df1267352b9740b5b0f604fa4dd975dac67749b7
Artur Taube <adduh@web.de>
2016-05-29 17:24:36 +0200

    Refactor tests.

--- test/game.js
+++ test/game.js
@@ -7,6 +7,7 @@ var Game = require('../src/game.js');
 describe('Game', () => {
   var game;
   beforeEach(() => { game = new Game(10); });
+
   describe('contructor()', () => {
     it('initiates running, players and time', () => {
       assert.equal(game.intervalId, undefined);
@@ -14,29 +15,30 @@ describe('Game', () => {
       assert.equal(game.time, 0);
     });
   });
-  describe('start()', () => {
-    it('sets the interval', () => {
-      game.start();
-      assert.notEqual(game.intervalId, undefined);
 
+  describe('start()', () => {
+    beforeEach(() => { game.start(); });
+
+    it('sets the interval', () => {
+      assert.notEqual(game.intervalId, undefined);
     });
+
     it('starts ticking', done => {
-      game.start();
       setTimeout(() => {
         assert.equal(game.time, 2);
         done();
       }, 2.5 * game.tickInterval);
     });
+
     it('does nothing if already running', done => {
       game.start();
-      game.start();
       setTimeout(() => {
         assert.equal(game.time, 2);
         done();
       }, 2.5 * game.tickInterval);
     });
+
     it('resumes after stop', done => {
-      game.start();
       setTimeout(() => {
         game.stop();
         setTimeout(() => {
@@ -46,33 +48,34 @@ describe('Game', () => {
         }, 2 * game.tickInterval);
       }, 2.5 * game.tickInterval);
     });
+
   });
   describe('stop()', () => {
-    it('clears the interval', done => {
+    beforeEach(done => {
       game.start();
       setTimeout(() => {
         game.stop();
-        assert.equal(game.intervalId, undefined);
         done();
       }, 2.5 * game.tickInterval);
     });
-    it('stops ticking', done => {
-      game.start();
-      setTimeout(() => {
-        game.stop();
-        setTimeout(() => {
-          assert.equal(game.intervalId, undefined);
-          assert.equal(game.time, 2);
-          done();
-        }, 2 * game.tickInterval);
-      }, 2.5 * game.tickInterval);
-    });
+
     it('does nothing if not running', () => {
-      game.start();
       game.stop();
-      game.stop();
+    });
+
+    it('clears the interval', () => {
+      assert.equal(game.intervalId, undefined);
+    });
+
+    it('stops ticking', done => {
+      setTimeout(() => {
+        assert.equal(game.intervalId, undefined);
+        assert.equal(game.time, 2);
+        done();
+      }, 2 * game.tickInterval);
     });
   });
+
   describe('addPlayer()', () => {
     it('adds a Player into list', () => {
       assert.equal(game.players.length, 0);
