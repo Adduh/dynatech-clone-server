2ca7385c9920af0382f3c153a7c082f14aac97e7
Artur Taube <adduh@web.de>
2016-06-05 19:43:13 +0200

    Player handles locations instead of producers.

--- src/player.js
+++ src/player.js
@@ -1,21 +1,26 @@
 'use strict';
 
 var log = require('./log.js');
+var Location = require('./location.js');
 
 class Player {
   constructor(name) {
-    this.producers = [];
+    this.locations = [];
     this.name = name ? name : 'Unnamed Player';
   }
 
-  addProducer(producer) {
-    log('Added producer to player \"%s\".', this.name);
-    this.producers.push(producer);
+  addLocation(location) {
+    if (!(location instanceof Location)) {
+      log('Could not add location to player \"%s\": Location is invalid');
+      return false;
+    }
+    log('Added location \"%s\" to player \"%s\".', location.name, this.name);
+    return this.locations.push(location);
   }
 
   tick() {
-    this.producers.forEach (producer => {
-      producer.tick();
+    this.locations.forEach (location => {
+      location.tick();
     });
   }
 }
--- test/player.js
+++ test/player.js
@@ -3,37 +3,44 @@
 var assert = require('assert');
 var sinon = require('sinon');
 
+var Location = require('../src/location.js');
 var Player = require('../src/player.js');
 
 describe('Player', () => {
   var player;
-  var producer;
+  var location;
   beforeEach(() => {
     player = new Player();
-    producer = { tick: sinon.spy() };
+    location = new Location();
+    location.tick = sinon.spy();
   });
 
   describe('constructor()', () => {
-    it('it initiates producers', () => {
-      assert.equal(player.producers.length, 0);
+    it('initiates locations', () => {
+      assert.equal(player.locations.length, 0);
     });
   });
 
-  describe('addProducer()', () => {
-    it('adds a producer to the producers list', () => {
-      assert.equal(player.producers.length, 0);
-      player.addProducer(producer);
-      assert.equal(player.producers.length, 1);
-      assert.equal(player.producers[0], producer);
+  describe('addLocation()', () => {
+    it('adds locations to the locations list', () => {
+      assert.equal(player.addLocation(location), 1);
+      assert.equal(player.addLocation(location), 2);
+      assert.equal(player.locations.length, 2);
+      assert.equal(player.locations[0], location);
+    });
+
+    it('does not add invalid Locations', () => {
+      assert.equal(player.addLocation({name: 'foo'}), false);
+      assert.equal(player.locations.length, 0);
     });
   });
 
   describe('tick()', () => {
-    it('calls tick on all producers', () => {
-      assert.equal(player.producers.length, 0);
-      player.addProducer(producer);
+    it('calls tick on all locations', () => {
+      assert.equal(player.locations.length, 0);
+      player.addLocation(location);
       player.tick();
-      assert.equal(producer.tick.called, true);
+      assert.equal(location.tick.called, true);
     });
   });
 });
